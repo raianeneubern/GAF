ec6d4a86a1e20bc5a26e3794383b7ccc75ba2bce
raianeneubern <raianeneubern@gmail.com>
2025-08-06 10:44:35 -0300

    Criado Projeto

---
+++ GAF.Api/Program.cs
@@ -0,0 +1,33 @@
+using System.Runtime.Intrinsics.Arm;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI( c =>
+    {
+        c.SwaggerEndPoint("/swagger/v1/swagger.json", "GAF API V1");
+        c.RoutePrefix = string.Empty; // Set Swagger UI at the app's root
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
